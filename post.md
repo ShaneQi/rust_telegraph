5560807d1eed48ce51e65c7d301282c5d81e3355
Shane Qi <qizengtai@gmail.com>
2017-09-21 15:53:14 -0500

    Switched to markdown-html.

---
+++ post.md
@@ -0,0 +1,57 @@
+title: hello
+
+---
+
+# Explicitly Set Time Zone Before Parsing Date from String in Swift
+
+> `Date` represents a single point in time. A `Date` is independent of a particular calendar or time zone.
+
+Let's say the API returned a date string to me: "2017-09-04 14:00:00". And the API document tells me that this date string is in UTC.
+
+Now I'm going to parse a Date from the string and store the date in my app's local database:
+
+```swift
+// CAUTION: this is a bad practice. 
+let dateFormatter = DateFormatter()
+dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+let date = dateFormatter.date(from: "2017-09-04 14:00:00")
+```
+
+This snippet of code is a bad practice in most cases, because the date string describes the date in UTC time zone, but the code parse date from it with the system time zone. (DateFormatter has system time zone as the default time zone)
+
+This bad practice turns into a bug when I format this date into string again:
+
+```swift
+// `date` is from code above.
+let dateFormatter = DateFormatter()
+dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+let dateString = dateFormatter.string(from: date)
+// dateString = "2017-09-04 14:00:00", but I'm expecting 
+// "2017-09-04 09:00:00" becuase I'm in -0500 time zone.
+```
+
+
+The formatting code doesn't have any problem, it uses a date formatter with system time zone to format a date. But the result is wrong because the date was parsed improperly at the first place.
+
+The solution is quite simple, explicitly set time zone before parsing date from string. The time zone should be the same as the one in which the date string describes date. Such that the date object I parsed stores the correct point in time.
+
+```swift
+// SHOULD DO
+let dateFormatter = DateFormatter()
+// Because API doc told me the date string is in UTC.
+dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+let date = dateFormatter.date(from: "2017-09-04 14:00:00")
+```
+
+Then when I want to present the date to user, I just use whichever time zone I need to format the date:
+
+```swift
+// `date` is from code above.
+let dateFormatter = DateFormatter()
+dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+// Not necessary to explicitly set time zone, because it has system time zone as default.
+// dateFormatter.timeZone = TimeZone(secondsFromGMT: -18000)
+let dateString = dateFormatter.string(from: date)
+// dateString = "2017-09-04 09:00:00", as I expected.
+```
--- src/main.rs
+++ src/main.rs
@@ -1,111 +1,35 @@
-#[macro_use]
-extern crate serde_derive;
-extern crate serde;
-#[macro_use]
-extern crate serde_json;
 extern crate handlebars;
+extern crate pulldown_cmark;
 
-extern crate futures;
-extern crate hyper;
-extern crate tokio_core;
-extern crate hyper_tls;
-
-use handlebars::Handlebars;
-use handlebars::{RenderContext, RenderError, Helper};
-use std::fs::{self, File};
+use pulldown_cmark::{Parser, html};
+use handlebars::{Handlebars, no_escape};
+use std::fs::File;
 use std::path::Path;
 use std::io::prelude::*;
-
-use futures::{Future, Stream};
-use hyper::{Client, Chunk};
-use tokio_core::reactor::Core;
-use hyper_tls::HttpsConnector;
-
-fn helper(h: &Helper, _: &Handlebars, rc: &mut RenderContext) -> Result<(), RenderError> {
-    let param = h.param(0).unwrap().value();
-    if let Some(str) = param.as_str() {
-        let rendered = format!("{}", str);
-        try!(rc.writer.write(rendered.into_bytes().as_ref()));
-    }
-    Ok(())
-}
-
-fn read_list() -> Vec<Page> {
-    let mut f = File::open("./content/list.json").expect("file not found");
-    let mut contents = String::new();
-    f.read_to_string(&mut contents).expect(
-        "something went wrong reading the file",
-    );
-    serde_json::from_str::<Vec<Page>>(&contents).unwrap()
-}
-
-fn get_json(page_path: String) -> serde_json::Value {
-    let mut core = Core::new().unwrap();
-    let client = Client::configure()
-        .connector(HttpsConnector::new(4, &core.handle()).unwrap())
-        .build(&core.handle());
-
-    let uri = format!(
-        "https://api.telegra.ph/getPage/{}?return_content=true",
-        page_path
-    ).parse()
-        .unwrap();
-    let work = client.get(uri).and_then(|res| {
-        res.body().concat2().and_then(|body: Chunk| {
-            serde_json::from_slice::<serde_json::Value>(&body).map_err(|_| hyper::Error::Incomplete)
-        })
-    });
-    let object = core.run(work);
-    return object.unwrap();
-}
+use std::collections::BTreeMap;
 
 fn main() {
-    let list = read_list();
     let mut handlebars = Handlebars::new();
-    handlebars.register_helper("plain", Box::new(helper));
+    handlebars.register_escape_fn(no_escape);
     // register template using given name
-    if let Err(e) = handlebars.register_template_file("list", "./list.hbs") {
-        panic!("{}", e);
-    }
-    if let Err(e) = handlebars.register_template_file("table", "./template.hbs") {
-        panic!("{}", e);
-    }
-    if let Err(e) = handlebars.register_template_file("page", "./page.hbs") {
+    if let Err(e) = handlebars.register_template_file("template", "./template.hbs") {
         panic!("{}", e);
     }
 
-    let list_html = handlebars.render("list", &json!(&list)).unwrap();
+    let mut data = BTreeMap::new();
+    data.insert("content".to_string(), read_file("post.md"));
+    let list_html = handlebars.render("template", &data).unwrap();
     let mut file = File::create(Path::new("./index.html")).unwrap();
     file.write_all(&list_html.as_bytes()).unwrap();
 
-    for page in list {
-
-        let page_path = &page.page_path;
-        let file_name = page.permalink.unwrap_or(page_path.to_string());
-        let file = format!("./{}/index.html", file_name);
-        let path = Path::new(&file);
-        let json = get_json(page_path.to_string());
-        let result = handlebars.render("table", &json).unwrap();
-
-        fs::create_dir(Path::new(&format!("./{}", file_name)));
-        let mut file = match File::create(&path) {
-            Err(_) => panic!("couldn't create {}", file_name),
-            Ok(file) => file,
-        };
-
-        print!("{}", result);
-        match file.write_all(&result.as_bytes()) {
-            Err(_) => panic!("couldn't write to {}", file_name),
-            Ok(_) => println!("successfully wrote to {}", file_name),
-        }
-    }
-
-
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-struct Page {
-    title: Option
-    permalink: Option<String>,
-    page_path: String,
+fn read_file(file_name: &str) -> String {
+    let mut file = File::open(file_name).expect("");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("");
+    let mut s = String::new();
+    let p = Parser::new(&contents);
+    html::push_html(&mut s, p);
+    return s;
 }
