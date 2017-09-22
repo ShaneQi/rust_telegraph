1fb005bb0a0421ce668ecc26f62e067916f7e83c
Shane Qi <qizengtai@gmail.com>
2017-09-21 20:52:21 -0500

    Added index list page.

--- src/main.rs
+++ src/main.rs
@@ -15,18 +15,29 @@ fn main() {
 
     let mut handlebars = Handlebars::new();
     handlebars.register_escape_fn(no_escape);
-    let _ = handlebars.register_template_file("template", "./templates/post.hbs");
+    let _ = handlebars.register_template_file("post", "./templates/post.hbs");
+    let _ = handlebars.register_template_file("index", "./templates/index.hbs");
+
+    let mut posts: Vec<BTreeMap<String, String>> = vec![];
 
     for dir_item in Path::new("./posts/").read_dir().expect("") {
         let path = dir_item.expect("").path();
         let data = read_file(path);
-        let html = handlebars.render("template", &data).expect("");
-        let file_name = data["title"].as_str().to_string().replace(" ", "-");
-        let _ = create_dir_all(&format!("./{}/", file_name));
-        File::create(&format!("./{}/index.html", file_name))
-            .and_then(|mut file| file.write_all(&html.as_bytes()))
+        let post_html = handlebars.render("post", &data).expect("");
+        let perma_name = data["perma_name"].as_str().to_string();
+        let _ = create_dir_all(&format!("./{}/", perma_name));
+        File::create(&format!("./{}/index.html", perma_name))
+            .and_then(|mut file| file.write_all(&post_html.as_bytes()))
             .expect("");
+        posts.push(data);
     }
+
+    posts.sort_by(|a, b| b["date"].as_str().cmp(a["date"].as_str()) );
+
+    let index_post_html = handlebars.render("index", &posts).expect("");
+    File::create("./index.html")
+        .and_then(|mut file| file.write_all(&index_post_html.as_bytes()))
+        .expect("");
 }
 
 fn read_file<P: AsRef<Path>>(path: P) -> BTreeMap<String, String> {
@@ -64,13 +75,13 @@ fn read_file<P: AsRef<Path>>(path: P) -> BTreeMap<String, String> {
     let yamls = YamlLoader::load_from_str(&yaml).expect("");
     let yaml_map = &yamls[0];
     let mut data = BTreeMap::new();
+    let title = yaml_map["title"].as_str().expect("").to_string();
+    let date = yaml_map["date"].as_str().expect("");
+    data.insert("date".to_string(), date.to_string());
+    data.insert("title".to_string(), title.clone());
     data.insert(
-        "title".to_string(),
-        yaml_map["title"].as_str().expect("").to_string(),
-    );
-    data.insert(
-        "date".to_string(),
-        yaml_map["date"].as_str().expect("").to_string(),
+        "perma_name".to_string(),
+        title.replace(" ", "-") + "-" + &date.chars().skip(2).take(8).collect::<String>(),
     );
     data.insert("content".to_string(), content);
     return data;
