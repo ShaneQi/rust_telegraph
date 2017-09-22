c87cf9941d4243b384ab3d1b4993fb8d38d803ae
Shane Qi <qizengtai@gmail.com>
2017-09-21 23:11:17 -0500

    Added input/output arguments.

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,27 @@ extern crate yaml_rust;
 use pulldown_cmark::{Parser, html};
 use yaml_rust::YamlLoader;
 use handlebars::{Handlebars, no_escape};
-use std::fs::{create_dir_all, File};
+use std::fs::{self, File};
 use std::path::Path;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::collections::BTreeMap;
+use std::env;
 
 fn main() {
+    // Get working paths.
+    let mut input_path = env::args().nth(1).unwrap_or("./".to_string());
+    let mut output_path = env::args().nth(2).unwrap_or("./".to_string());
+
+    // Check if path has trailing slash.
+    if input_path.chars().rev().nth(0).unwrap() != '/' {
+        input_path += "/";
+    }
+    if output_path.chars().rev().nth(0).unwrap() != '/' {
+        output_path += "/";
+    }
+    println!("Input path: {}", input_path);
+    println!("Onput path: {}", output_path);
 
     // Config handlerbars.
     let mut handlebars = Handlebars::new();
@@ -21,17 +35,24 @@ fn main() {
 
     // Process md files.
     let mut posts: Vec<BTreeMap<String, String>> = vec![];
-    for dir_item in Path::new("./posts/").read_dir().expect("") {
+    for dir_item in Path::new(&format!("{}posts/", input_path))
+        .read_dir()
+        .expect(&format!(
+            "Failed to find post directory: {}posts/",
+            input_path
+        ))
+    {
         let path = dir_item.expect("").path();
         if path.extension().and_then(|s| s.to_str()).unwrap_or("") != "md" {
+            println!("Skipping file: {:?}", path);
             continue;
         }
-        println!("Processed file: {:?}", path);
+        println!("Processing file: {:?}", path);
         let data = read_file(path);
         let post_html = handlebars.render("post", &data).expect("");
         let permalink = data["permalink"].as_str().to_string();
-        let _ = create_dir_all(&format!("./{}/", permalink));
-        File::create(&format!("./{}/index.html", permalink))
+        let _ = fs::create_dir_all(&format!("{}{}/", output_path, permalink));
+        File::create(&format!("{}{}/index.html", output_path, permalink))
             .and_then(|mut file| file.write_all(&post_html.as_bytes()))
             .expect("");
         posts.push(data);
@@ -40,9 +61,16 @@ fn main() {
 
     // Generate homepage.
     let index_post_html = handlebars.render("index", &posts).expect("");
-    File::create("./index.html")
+    File::create(&format!("{}index.html", output_path))
         .and_then(|mut file| file.write_all(&index_post_html.as_bytes()))
         .expect("");
+
+    // Copy assets.
+    let _ = fs::create_dir_all(&format!("{}css/", output_path));
+    fs::copy(
+        "./assets/css/telegraph.css",
+        &format!("{}css/telegraph.css", output_path),
+    ).expect("");
 }
 
 fn read_file<P: AsRef<Path>>(path: P) -> BTreeMap<String, String> {
@@ -92,7 +120,8 @@ fn read_file<P: AsRef<Path>>(path: P) -> BTreeMap<String, String> {
             .as_str()
             .map(|s| s.to_string())
             .unwrap_or(
-                title.to_lowercase().replace(" ", "-") + "-" + &date.chars().skip(2).take(8).collect::<String>(),
+                title.to_lowercase().replace(" ", "-") + "-" +
+                    &date.chars().skip(2).take(8).collect::<String>(),
             ),
     );
     data.insert("content".to_string(), content);
